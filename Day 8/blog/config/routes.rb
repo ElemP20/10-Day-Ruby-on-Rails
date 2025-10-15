ef7081cfaf3322a6b05505b157a9856288ecdaef
Flloyd Nathaniel Gagni <fridvinda@gmail.com>
2025-10-15 14:14:17 +0800

    Define routes and nested routes for association

--- Day 8/blog/config/routes.rb
+++ Day 8/blog/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get "comments/create"
-  get "posts/index"
-  get "posts/show"
-  get "posts/new"
-  get "posts/create"
+  resources :posts do
+    resources :comments, only: [:create]
+  end
+
+  root "posts#index"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
