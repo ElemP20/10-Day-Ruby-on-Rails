0e3eb33c7830027b58ddee5b6e8a4c87bb40f52e
Flloyd Nathaniel Gagni <fridvinda@gmail.com>
2025-10-15 21:27:41 +0800

    Create user model

---
+++ Day 9/airlines/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  enum role: { economy: 0, business: 1, first_class: 2 }
+
+  validates :email, presence: true, uniqueness: true
+  validates :role, presence: true
+end
