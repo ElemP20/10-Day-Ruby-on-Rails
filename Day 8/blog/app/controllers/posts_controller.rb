de7cd8c07beae3796ae24d8b64c350f605fb2159
Flloyd Nathaniel Gagni <fridvinda@gmail.com>
2025-10-15 14:14:03 +0800

    Define the actions in the controller

--- Day 8/blog/app/controllers/posts_controller.rb
+++ Day 8/blog/app/controllers/posts_controller.rb
@@ -1,13 +1,30 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
