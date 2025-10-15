7c29c5c77a75bff8a1665fb801b521c03701f14a
Flloyd Nathaniel Gagni <fridvinda@gmail.com>
2025-10-16 06:53:57 +0800

    Define user controller actions and private methods

--- Day 9/airlines/app/controllers/users_controller.rb
+++ Day 9/airlines/app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Account created and logged in!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Error creating account. Please check the form."
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :role) 
+    end
 end
