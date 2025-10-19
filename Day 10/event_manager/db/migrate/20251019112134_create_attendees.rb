619e00a8c58e480aae915e03e9a86f4576c0f93c
Flloyd Nathaniel Gagni <fridvinda@gmail.com>
2025-10-19 19:21:47 +0800

    Create Attendee model

---
+++ Day 10/event_manager/db/migrate/20251019112134_create_attendees.rb
@@ -0,0 +1,12 @@
+class CreateAttendees < ActiveRecord::Migration[8.0]
+  def change
+    create_table :attendees do |t|
+      t.string :name
+      t.string :email
+      t.string :organization
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
